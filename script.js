4b0609d894c84905efd173d7a91746d27b4d6bfa
khalil el kadih <khalil94elkadih@gmail.com>
2022-01-17 09:09:11 +0000

    Update

---
+++ script.js
@@ -0,0 +1,15 @@
+function OpenPage() {
+
+    if (document.getElementById("ToOpenPage").value == "user") {
+        alert("hello e learn");
+
+    } else
+    if (document.getElementById("ToOpenPage").value == "admin") {
+        alert("hello e learn");
+        window.open("../dashbord/index.html");
+    } else {
+
+        alert("wrong XD");
+    }
+}
+alert("hiiiiiiiiii");
