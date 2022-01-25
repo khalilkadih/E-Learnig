5ce8cb817b2de6a551bb287dfe1e1b75b5130166
khalil el kadih <khalil94elkadih@gmail.com>
2022-01-25 20:16:42 +0000

    Sidebar update

--- dashbord/sidebar.php
+++ dashbord/sidebar.php
@@ -1,3 +1,14 @@
+<?php 
+  $active='btn-primary';
+ 
+    $page= basename($_SERVER['SCRIPT_FILENAME']);
+    function highlightIfACtive($p){
+        global $page,$active;
+        if($p==$page)
+        echo $active;
+    }
+  
+  ?>
   <div class="col-2 sidebar d-flex align-items-center flex-column vh-100">
                 <h1 class=" mt-3 text-start border-start border-5 border-primary ps-1 align-self-start mb-5 fs-6 fw-bold" id="changeTailleOfH1">E-Classe</h1>
                 <img src="../img/logo.jfif " class="w-50 img-profil" alt="photo profile">
@@ -8,16 +19,16 @@
                 <nav class="w-75 mx-auto" title="mainPrincipale">
                     <ul class="w-100 mt-5 menu-items list-unstyled">
                         <li>
-                            <a href="dashboard.php" class="btn btn-primary w-100 active"> <span class="ic ic-home"></span> <span>Home</span> </a>
+                            <a href="dashboard.php" class="btn <?php highlightIfACtive('dashboard.php')?> w-100 active"> <span class="ic ic-home"></span> <span>Home</span> </a>
                         </li>
                         <li>
-                            <a href="#" class="btn  w-100"> <span class="ic ic-course"></span><span>Courses</span></a>
+                            <a href="#" class="btn  w-100"> <span class="ic ic-course" ></span><span>Courses</span></a>
                         </li>
                         <li>
-                            <a href="students.php" class="btn    w-100"> <span class=" ic ic-student "></span><span>Student</span></a>
+                            <a href="students.php" class="btn  <?php highlightIfACtive('students.php')?>  w-100"> <span class=" ic ic-student "></span><span>Student</span></a>
                         </li>
                         <li>
-                            <a href="payments.php" class="btn    w-100"> <span class=" ic ic-payment "></span><span>Payment</span></a>
+                            <a href="payments.php" class="btn <?php highlightIfACtive('payments.php')?>   w-100"> <span class=" ic ic-payment "></span><span>Payment</span></a>
                         </li>
                         <li>
                             <a href="# " class="btn    w-100"> <span class=" ic ic-report "></span> <span>Report</span></a>
