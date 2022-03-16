00aeaf6e18651894bac7bb6f5eaf9e25b02e4c2d
khalil el kadih <khalil94elkadih@gmail.com>
2022-03-16 13:53:20 +0000

    create page sign up

--- sign_up.php
+++ sign_up.php
@@ -20,40 +20,57 @@
                         <h1 class="text-start border-start border-5 border-primary ps-1 fw-bold">E-Classe</h1>
                         <h2 class="h3 text-center text-uppercase mb-3 fw-bold">Sign up </h2>
                         <p class="text-center "> Create New Acount </p>
-                            <div class="" style="display:flex;    justify-content:space-around; margin :20px !important; ">
+                            <!-- <div class="" style="display:flex;    justify-content:space-around; margin :20px !important; ">
                             
                                 <a href="" class="btn  btn-primary  fw-bold " style="margin-top: .5rem !important; width:40%;"> Sign up</a>
                                 <a href="" class="btn btn-primary mt-2 fw-bold" style="margin-top: .5rem !important; width:40%"> Sign up</a>
-                            </div>
+                            </div> -->
 
-                            <form method="POST" action="../dashbord/dashboard.php">
-                                <div class="mb-3 ">
-                                    <label for="Username" class="form-label">Username:</label>
-                                    <input type="Username" class="form-control " placeholder="Enter your username" id="Username">
-
+                            <form method="POST" action="signUpSubmit.php" id="form">
+                                <div class="mb-3 form-control ">
+                                    <!-- pattern="^[a-zA-Z]+\s[a-zA-Z]+$" title="Check your first Name" -->
+                                    <label for="firstName" class="form-label">first name:</label>
+                                    <input type="text" class="form-control"  name="firstName" placeholder="Enter your first name" id="firstName" required>
+                                    <small>Error</small>
                                 </div>
-                                <div class="mb-3 ">
-                                    <label for="ToOpenPage" class="form-label">Email:</label>
-                                    <input type="Email" class="form-control " placeholder="Enter your Email" id="ToOpenPage">
-
+                                <div class="mb-3  form-control">
+                                    <!-- pattern="^[a-zA-Z]+\s[a-zA-Z]+$" title="Check your Last Name" -->
+                                    <label for="LastName" class="form-label">Last Name:</label>
+                                    <input type="text" class="form-control"  name="LastName"  placeholder="Enter your LastName" id="LastName" required>
+                                    <small>Error</small>
                                 </div>
-                                <div class="mb-3 ">
+                                <div class="mb-3 form-control ">
+                                    <!-- placeholder="khalil.99@example.com" patter n="/\w+\.+\w+@+\w+\.+com/gm" title="check your Email" -->
+                                    <label for="email" class="form-label">Email:</label>
+                                    <input type="email" class="form-control" placeholder="jhonDoe@example.com" name="email"  id="email" required>
+                                    <small>Error</small>
+                                </div>
+                                <div class="mb-3 form-control ">
+                                <!-- pattern="(?=.*[0-9])(?=.*[A-Z]^.{8,})$" title="You must a caractere"  -->
                                     <label for="Password" class="form-label">Password:</label>
-                                    <input type="Password" class="form-control " placeholder="Enter your Password" id="Password">
-
+                                    <input type="password" class="form-control " name="password" placeholder="Enter your Password" id="Password" required>
+                                    <small>Error</small>
                                 </div>
-                                <div class="mb-3">
-                                    <label for="PasswortInput" class="form-label mt-1"> Confirm Password :</label>
-                                    <input type="password" placeholder="Confirm  Password" class="form-control" id="PasswortInput">
+                                <div class="mb-3 form-control">
+                                    <label for="confirmPwd" class="form-label mt-1"> Confirm Password :</label>
+                                    <input type="password" placeholder="Confirm  Password"  name="confirmPwd" class="form-control" id="confirmPwd" required>
+                                    <small>Error</small>
                                 </div>
-
                                 <button type="submit" class="btn btn-primary w-100 fw-bold">SIGN IN</button>
                             </form>
                     </div>
-                    <span class="text-center mb-5 mt-.5 ">forget your password ? <a href="# ">Reset password</a></span>
-                </div>
+                        <div class="text-center"> 
+                                    <p class="fw-bold">
+                                    If already have account sign in here
+                                    </p>
+                                    <a href="index.php" class=" btn btn-primary w-50 mb-4">Sign in</a></span>
+
+                        </div>
+                        <span class="text-center mb-5 mt-.5 ">forget your password ? <a href="# ">Reset password</a></span>
+                    </div>
             </div>
         </section>
+        
     </main>
     <script src="script.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js " integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB " crossorigin="anonymous"></script>
