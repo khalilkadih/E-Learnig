699af1034dc98d6984fe3e19b26e3d8624870db0
khalil el kadih <khalil94elkadih@gmail.com>
2022-03-16 22:28:06 +0000

    backup

--- script.js
+++ script.js
@@ -1,34 +1,81 @@
-// document.forms[0].onsubmit = function(e) {
-//     e.preventDefault();
-//     // window.open("../dashbord/Dashbord.html");
-//     window.location.href = "../dashbord/dashboard.php";
-// }
+    // document.forms[0].onsubmit = function(e) {
+    //     e.preventDefault();
+    //     // window.open("../dashbord/Dashbord.html");
+    //     window.location.href = "../dashbord/dashboard.php";
+    // }
 
 
+    const form = document.getElementById('form');
+    const firstName=document.getElementById('firstName');
+    const LastName=document.getElementById('LastName');
+    const email=document.getElementById('email');
+    const password=document.getElementById('password');
+    const password_conf=document.getElementById('confirmPwd');
 
-function showerror(input ,message){
-const formControll=input.parentElemnt;
-formControll.className='form-control error';
-const small=document.querySelector('small');
-small.innerText=message;
+    function showerror(input ,message){
+
+        const formControll=input.parentElement;
+        formControll.className='form-control error';
+        const small=formControll.querySelector('small');
+        small.innerText=message;
+    }
+    function showSuccess(input){
+        const formControl=input.parentElement;
+        formControl.className='form-control success';
+    }
+    function checkPassworMatch(input1 ,input2,ev){
+        if(input1.value !==input2.value){
+            showerror(input2,'password not Match ');
+            ev.preventDefault();
+        }
+        
+    }
+  
 
 
-}
-const form = document.getElementById('form');
-const firstName=document.getElementById('firstName');
-const lastName=document.getElementById('LastName');
-const email=document.getElementById('email');
-const password=document.getElementById('Password');
-const password_conf=document.getElementById('confirmPwd');
+    form.addEventListener('submit',function(e){
+            
+        // e.preventDefault();
+
+            if(firstName.value==="")
+        {
+            showerror(firstName,'first Name required');
+
+            e.preventDefault();
+        }else{
+            showSuccess(firstName);
+        }
+        if(LastName.value==="")
+        {
+        showerror(LastName,'first Name required');
+        e.preventDefault();
+    }else{
+        showSuccess(LastName);
+    }
+    if(email.value==="")
+    {
+        showerror(email,'first Name required');
+        e.preventDefault();
+    }else{
+        showSuccess(email);
+    }
+    if(firstName.value==="")
+    {
+        showerror(password,'first Name required');
+        e.preventDefault();
+        }else{
+        showSuccess(password);
+    }
+    if(firstName.value==="")
+    {
+        showerror(password_conf,'first Name required');
+        e.preventDefault();
+
+    }else{
+        showSuccess(password_conf);
+    }
+
+    checkPassworMatch(password, password_conf,e);
 
 
-form.addEventListener('submit',function(e){
-e.preventDefault();
-
-if(firstName.value==="")
-{
-showerror(firstName,'champ obligatoire');
-}
-
-
-})
+    })
--- sign_up.php
+++ sign_up.php
@@ -1,80 +1,60 @@
 
-<!DOCTYPE html>
-<html lang="en">
+    <!DOCTYPE html>
+    <html lang="en">
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet " href="style.css ">
-    <title>E-Learning</title>
-</head>
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet " href="style.css ">
+        <title>E-Learning</title>
+    </head>
 
-<body>
-    <main class="container-fluid ">
-        <section class="row ">
-            <div class="col-12 col-xl-4 offset-xl-4 col-lg-6 offset-lg-3 mt-5 pt-5">
-                <div class="card shadow ">
-                    <div class="card-body p-5 ">
-                        <h1 class="text-start border-start border-5 border-primary ps-1 fw-bold">E-Classe</h1>
-                        <h2 class="h3 text-center text-uppercase mb-3 fw-bold">Sign up </h2>
-                        <p class="text-center "> Create New Acount </p>
-                            <!-- <div class="" style="display:flex;    justify-content:space-around; margin :20px !important; ">
-                            
-                                <a href="" class="btn  btn-primary  fw-bold " style="margin-top: .5rem !important; width:40%;"> Sign up</a>
-                                <a href="" class="btn btn-primary mt-2 fw-bold" style="margin-top: .5rem !important; width:40%"> Sign up</a>
-                            </div> -->
+    <body>
+    <main class="container">
+            <form method="POST"  id="form">
+                <h2>Register with us</h2>
+                <div class=" form-control">
+                    <label for="firstName ">first Name:</label>
+                    <input type="text " placeholder="Enter first name " name="firstName" id="firstName ">
+                    <small>Error message</small>
+                </div>
+                <div class=" form-control">
+                    <label for="LastName ">Last Name:</label>
+                    <input type="text " placeholder="Enter LastName " name="LastName" id="LastName ">
+                    <small>Error message</small>
+                </div>
+                <div class="form-control">
+                    <label for="email ">Email:</label>
+                    <input type="email " placeholder="Enter Email " name="email" id="email ">
+                    <small>Error message</small>
+                </div>
 
-                            <form method="POST" action="signUpSubmit.php" id="form">
-                                <div class="mb-3 form-control ">
-                                    <!-- pattern="^[a-zA-Z]+\s[a-zA-Z]+$" title="Check your first Name" -->
-                                    <label for="firstName" class="form-label">first name:</label>
-                                    <input type="text" class="form-control"  name="firstName" placeholder="Enter your first name" id="firstName" required>
-                                    <small>Error</small>
-                                </div>
-                                <div class="mb-3  form-control">
-                                    <!-- pattern="^[a-zA-Z]+\s[a-zA-Z]+$" title="Check your Last Name" -->
-                                    <label for="LastName" class="form-label">Last Name:</label>
-                                    <input type="text" class="form-control"  name="LastName"  placeholder="Enter your LastName" id="LastName" required>
-                                    <small>Error</small>
-                                </div>
-                                <div class="mb-3 form-control ">
-                                    <!-- placeholder="khalil.99@example.com" patter n="/\w+\.+\w+@+\w+\.+com/gm" title="check your Email" -->
-                                    <label for="email" class="form-label">Email:</label>
-                                    <input type="email" class="form-control" placeholder="jhonDoe@example.com" name="email"  id="email" required>
-                                    <small>Error</small>
-                                </div>
-                                <div class="mb-3 form-control ">
-                                <!-- pattern="(?=.*[0-9])(?=.*[A-Z]^.{8,})$" title="You must a caractere"  -->
-                                    <label for="Password" class="form-label">Password:</label>
-                                    <input type="password" class="form-control " name="password" placeholder="Enter your Password" id="Password" required>
-                                    <small>Error</small>
-                                </div>
-                                <div class="mb-3 form-control">
-                                    <label for="confirmPwd" class="form-label mt-1"> Confirm Password :</label>
-                                    <input type="password" placeholder="Confirm  Password"  name="confirmPwd" class="form-control" id="confirmPwd" required>
-                                    <small>Error</small>
-                                </div>
-                                <button type="submit" class="btn btn-primary w-100 fw-bold">SIGN IN</button>
-                            </form>
-                    </div>
+                <div class="form-control">
+                    <label for="password">Password:</label>
+                    <input  type="password" placeholder="Enter Password" name="password" id="password ">
+                    <small>Error message</small>
+
+                </div>
+
+                <div class="form-control">
+                    <label for="confirmPwd ">Confirm password:</label>
+                    <input type="password" placeholder="Confirmer Password " name="confirmPwd" id="confirmPwd ">
+                    <small>Error message</small>
+                </div>
+
+                <button type="submit ">Submit</button>
+            </form>
                         <div class="text-center"> 
-                                    <p class="fw-bold">
-                                    If already have account sign in here
-                                    </p>
-                                    <a href="index.php" class=" btn btn-primary w-50 mb-4">Sign in</a></span>
+                                        <p class="fw-bold">
+                                        If already have account sign in here
+                                        </p>
+                                        <a href="" class=" btn btn-primary w-50 mb-4">Sign in</a></span>
 
-                        </div>
-                        <span class="text-center mb-5 mt-.5 ">forget your password ? <a href="# ">Reset password</a></span>
-                    </div>
-            </div>
-        </section>
-        
-    </main>
-    <script src="script.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js " integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB " crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js " integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13 " crossorigin="anonymous"></script>
-</body>
+                            </div>
+        </main>
 
-</html>
+<script src="script.js"></script>    
+    </body>
+
+    </html>
